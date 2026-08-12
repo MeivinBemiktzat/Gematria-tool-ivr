6a8a5dd5a5b4d4bf2319abdc1bcab120ef1b5909
מייבין במקצת <y15761576@gmail.com>
2026-08-12 21:04:58 +0300

    Update index.js

--- api/yemot/index.js
+++ api/yemot/index.js
@@ -443,7 +443,7 @@ async function handleStep(state, query, stateKey, res, did, systemToken) {
                 transitionTo(stateKey, state, 'TYPE_NAME');
                 return respond(res, buildRead({
                     mbId: MB.ASK_TYPE_NAME_TEXT,
-                    ttsText: 'אנא הקלידו את שמכם באמצעות מקלדת ההקלדה, ובסיום הקישו סולמית.',
+                    ttsText: 'אנא הקלידו את שמכם באמצעות מקשי הפלאפון, בין אות לאות הקישו סולמית, ובסיום ההקלדה הקישו כוכבית וסולמית.',
                     mode: 'tap',
                     maxDigits: 30,
                     minDigits: 1,
@@ -779,7 +779,7 @@ async function handleStep(state, query, stateKey, res, did, systemToken) {
                 transitionTo(stateKey, state, 'TYPE_EMAIL');
                 return respond(res, buildRead({
                     mbId: MB.TYPE_EMAIL,
-                    ttsText: 'אנא הקלידו את כתובת המייל באמצעות המקלדת, ובסיום הקישו סולמית.',
+                    ttsText: 'אנא הקלידו את שמכם באמצעות מקשי הפלאפון, בין אות לאות הקישו סולמית, ובסיום ההקלדה הקישו כוכבית וסולמית.',
                     mode: 'tap',
                     maxDigits: 60,
                     minDigits: 3,
